69838ec834ea6ec439ee2b3c15e19023032f6637
aya <2003ivnva@gmail.com>
2022-10-11 02:57:06 +0900

    Updates

---
+++ js/6078.a4d0566e.js
@@ -0,0 +1,6 @@
+"use strict";(self["webpackChunktest"]=self["webpackChunktest"]||[]).push([[6078],{6078:function(t,e,n){n.r(e),n.d(e,{startStatusTap:function(){return i}});var s=n(3087),r=n(4074),o=n(6587);
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+const i=()=>{const t=window;t.addEventListener("statusTap",(()=>{(0,s.wj)((()=>{const e=t.innerWidth,n=t.innerHeight,i=document.elementFromPoint(e/2,n/2);if(!i)return;const a=(0,r.a)(i);a&&new Promise((t=>(0,o.c)(a,t))).then((()=>{(0,s.Iu)((async()=>{a.style.setProperty("--overflow","hidden"),await(0,r.s)(a,300),a.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=6078.a4d0566e.js.map
